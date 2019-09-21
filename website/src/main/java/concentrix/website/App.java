e0e3389aa8111c2168b7da0110813c3e076c167b
Mayank <mayank.sharma2409@gmail.com>
2019-09-21 13:29:09 +0530

    FIrst commit

---
+++ website/src/main/java/concentrix/website/App.java
@@ -0,0 +1,13 @@
+package concentrix.website;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
